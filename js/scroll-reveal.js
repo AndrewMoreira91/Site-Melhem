6bc3dcab53870774a1fcd406a2e003051ed2df2c
Andrew Moreira <andrewsantos9113@gmail.com>
2023-11-20 21:50:14 -0300

    Correções de bugs

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -1,30 +1,24 @@
 window.sr = ScrollReveal({ reset:true });
 
-sr.reveal('#card-resumo',{
-    duration: 2500,
-    rotate: {x: 0,  y:80, z:0}
-});
-
 sr.reveal('#resumos',{
     duration: 2000,
     rotate: {x: 0,  y:80, z:0}
 });
 
+// sr.reveal('#quem-somos',{
+//     duration: 2000,
+//     rotate: {x: 80,  y:0, z:0}
+// });
 
-sr.reveal('#quem-somos',{
-    duration: 2000,
-    rotate: {x: 80,  y:0, z:0}
-});
+// sr.reveal('#experimente',{
+//     duration: 2000,
+//     rotate: {x: 80,  y:0, z:0}
+// });
 
-sr.reveal('#experimente',{
-    duration: 2000,
-    rotate: {x: 80,  y:0, z:0}
-});
-
-sr.reveal('#imoveis-destaque',{
-    duration: 2000,
-    rotate: {x: 80,  y:0, z:0}
-});
+// sr.reveal('#imoveis-destaque',{
+//     duration: 2000,
+//     rotate: {x: 80,  y:0, z:0}
+// });
 
 sr.reveal('#box-login',{
     duration: 1500,
